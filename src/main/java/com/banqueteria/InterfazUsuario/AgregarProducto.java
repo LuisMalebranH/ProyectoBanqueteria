cb0382eda42fde40d5f623c5670a8d409c97ec3e
UsuarioCompuElite <UsuarioCompuElite@-PC>
2025-05-14 05:00:29 -0400

    y mas

--- src/main/java/com/banqueteria/InterfazUsuario/AgregarProducto.java
+++ src/main/java/com/banqueteria/InterfazUsuario/AgregarProducto.java
@@ -262,6 +262,8 @@ public class AgregarProducto extends javax.swing.JFrame {
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
 //        dispose();
 //        this.setVisible(true);
+//        llenarTabla();
+//        llenarCategorias();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
